85e90910ff5351592ea111aaf13d30b55dd2de7a
Fernando Teodoro <fteodorodeoliveira@gmail.com>
2022-04-05 11:20:48 -0300

    Criação da base do projeto.
    Obs.: usei como base o esqueleto do projeto fornecido pela Base2 durante o Base2Camp e fazendo as adaptações necessárias para esse projeto.

---
+++ src/test/java/com/javaseleniumtemplate/tests/LoginTests.java
@@ -0,0 +1,24 @@
+package com.javaseleniumtemplate.tests;
+
+import com.javaseleniumtemplate.bases.TestBase;
+import com.javaseleniumtemplate.pages.LoginPage;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTests extends TestBase {
+    //Objects
+    LoginPage loginPage;
+
+    //Tests
+    @Test
+    public void efetuarLoginEValidarUsuario(){
+
+        //Objects instances
+        loginPage = new LoginPage();
+
+        //Parameteres
+
+        //Test
+
+    }
+}
